e23a93d698c8e248dcea91166d7aee4f21bd4148
mm <user@Mohamed-Khalefas-MacBook-Air.local>
2019-09-10 14:22:51 -0400

    expr  #Please enter the commit message for your changes. Lines starting

--- expr/run.sh
+++ expr/run.sh
@@ -6,13 +6,13 @@ echo "Auth"
 echo  $VARIABLE
 
 
-./v1dim ../data/EWords ../results/words_ ../data/wiki10 $VARIABLE 0 100
-./v1dim ../data/EWords ../results/words_ ../data/wiki10 $VARIABLE 100 200
-./v1dim ../data/EWords ../results/words_ ../data/wiki10 $VARIABLE 200 300
-./v1dim ../data/EWords ../results/words_ ../data/wiki10 $VARIABLE 300 400
-./v1dim ../data/EWords ../results/words_ ../data/wiki10 $VARIABLE 400 500
-./v1dim ../data/EWords ../results/words_ ../data/wiki10 $VARIABLE 500 600
-./v1dim ../data/EWords ../results/words_ ../data/wiki10 $VARIABLE 600 700
+#./v1dim ../data/EWords ../results/words_ ../data/wiki10 $VARIABLE 0 100
+#./v1dim ../data/EWords ../results/words_ ../data/wiki10 $VARIABLE 100 200
+#./v1dim ../data/EWords ../results/words_ ../data/wiki10 $VARIABLE 200 300
+#./v1dim ../data/EWords ../results/words_ ../data/wiki10 $VARIABLE 300 400
+#./v1dim ../data/EWords ../results/words_ ../data/wiki10 $VARIABLE 400 500
+#./v1dim ../data/EWords ../results/words_ ../data/wiki10 $VARIABLE 500 600
+#./v1dim ../data/EWords ../results/words_ ../data/wiki10 $VARIABLE 600 700
 
 ./v2dim ../data/EWords ../results/words_ ../data/wiki10 $VARIABLE 0 100
 ./v2dim ../data/EWords ../results/words_ ../data/wiki10 $VARIABLE 100 200
@@ -23,13 +23,13 @@ echo  $VARIABLE
 ./v2dim ../data/EWords ../results/words_ ../data/wiki10 $VARIABLE 600 700
 
 
-./v1dim ../data/Auth1M ../results/auth_ ../data/Auth10 $VARIABLE 0 99
-./v1dim ../data/Auth1M ../results/auth_ ../data/Auth10 $VARIABLE 100 200
-./v1dim ../data/Auth1M ../results/auth_ ../data/Auth10 $VARIABLE 200 300
-./v1dim ../data/Auth1M ../results/auth_ ../data/Auth10 $VARIABLE 300 400
-./v1dim ../data/Auth1M ../results/auth_ ../data/Auth10 $VARIABLE 400 500
-./v1dim ../data/Auth1M ../results/auth_ ../data/Auth10 $VARIABLE 500 600 
-./v1dim ../data/Auth1M ../results/auth_ ../data/Auth10 $VARIABLE 600 660
+#./v1dim ../data/Auth1M ../results/auth_ ../data/Auth10 $VARIABLE 0 99
+#./v1dim ../data/Auth1M ../results/auth_ ../data/Auth10 $VARIABLE 100 200
+#./v1dim ../data/Auth1M ../results/auth_ ../data/Auth10 $VARIABLE 200 300
+#./v1dim ../data/Auth1M ../results/auth_ ../data/Auth10 $VARIABLE 300 400
+#./v1dim ../data/Auth1M ../results/auth_ ../data/Auth10 $VARIABLE 400 500
+#./v1dim ../data/Auth1M ../results/auth_ ../data/Auth10 $VARIABLE 500 600 
+#./v1dim ../data/Auth1M ../results/auth_ ../data/Auth10 $VARIABLE 600 660
 
 
 ./v2dim ../data/Auth1M ../results/auth_ ../data/Auth10 $VARIABLE 0 99
